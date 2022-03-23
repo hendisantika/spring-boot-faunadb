a8cc6301b75c42642658a0c22c3512d8376ba267
Hendi Santika <hendisantika@gmail.com>
2022-03-23 07:15:38 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-faunadb
---
+++ src/main/java/com/hendisantika/SpringBootFaunadbApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFaunadbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFaunadbApplication.class, args);
+    }
+
+}
