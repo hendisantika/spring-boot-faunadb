f59fa705cb50066bee50b0aadb7990881d231d96
Hendi Santika <hendisantika@gmail.com>
2022-03-23 08:01:37 +0700

    #8 - Add Author Record

---
+++ src/main/java/com/hendisantika/post/Author.java
@@ -0,0 +1,13 @@
+package com.hendisantika.post;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-faunadb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/03/22
+ * Time: 08.00
+ */
+public record Author(String username, String name) {
+}
