ec54bab27e7e1ede4bbe3ade2bc53336e3f20b1f
Hendi Santika <hendisantika@gmail.com>
2022-03-23 08:07:51 +0700

    #21 - Add createPost API in PostsController

--- src/main/java/com/hendisantika/post/PostsController.java
+++ src/main/java/com/hendisantika/post/PostsController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -35,4 +41,13 @@ public class PostsController {
             throws ExecutionException, InterruptedException {
         return postsService.getPost(id, before);
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("isAuthenticated()")
+    public void createPost(@RequestBody UpdatedPost post) throws ExecutionException, InterruptedException {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        postsService.createPost(name, post.title(), post.content());
+    }
+
 }
