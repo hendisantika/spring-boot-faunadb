96db1b98304fbc8f5caedeef64b47718de4415f3
Hendi Santika <hendisantika@gmail.com>
2022-03-23 08:03:50 +0700

    #12 - Add getPost in PostsService

--- src/main/java/com/hendisantika/post/PostsService.java
+++ src/main/java/com/hendisantika/post/PostsService.java
@@ -4,6 +4,10 @@ import com.faunadb.client.FaunaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutionException;
+
+import static com.faunadb.client.query.Language.*;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-faunadb
@@ -18,4 +22,22 @@ public class PostsService {
     @Autowired
     private FaunaClient faunaClient;
 
+    Post getPost(String id, Long before) throws ExecutionException, InterruptedException {
+        var query = Get(Ref(Collection("posts"), id));
+        if (before != null) {
+            query = At(Value(before - 1), query);
+        }
+
+        var postResult = faunaClient.query(
+                Let(
+                        "post", query
+                ).in(
+                        Obj(
+                                "post", Var("post"),
+                                "author", Get(Select(Arr(Value("data"), Value("authorRef")), Var("post")))
+                        )
+                )).get();
+
+        return parsePost(postResult);
+    }
 }
