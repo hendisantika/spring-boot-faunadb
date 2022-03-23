0611fc1ad621ccb5b8e62750e8651f1ea121311c
Hendi Santika <hendisantika@gmail.com>
2022-03-23 07:28:40 +0700

    #7 - Add WebSecurityConfiguration

---
+++ src/main/java/com/hendisantika/config/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.hendisantika.config;
+
+import com.faunadb.client.FaunaClient;
+import com.hendisantika.user.FaunaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-faunadb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/03/22
+ * Time: 07.28
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private FaunaClient faunaClient;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new FaunaUserDetailsService(faunaClient);
+    }
+}
