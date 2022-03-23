fa2ff982924f1d05d780d26e6dcea2b8373ef8ae
Hendi Santika <hendisantika@gmail.com>
2022-03-23 08:08:13 +0700

    #22 - Add updatePost API in PostsController

--- src/main/java/com/hendisantika/post/PostsController.java
+++ src/main/java/com/hendisantika/post/PostsController.java
@@ -4,14 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -48,6 +41,14 @@ public class PostsController {
     public void createPost(@RequestBody UpdatedPost post) throws ExecutionException, InterruptedException {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         postsService.createPost(name, post.title(), post.content());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("isAuthenticated()")
+    public void updatePost(@PathVariable("id") String id, @RequestBody UpdatedPost post)
+            throws ExecutionException, InterruptedException {
+        postsService.updatePost(id, post.title(), post.content());
     }
 
 }
