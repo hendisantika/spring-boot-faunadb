fcea1209c9c0ef5e2b7cd0ec95a18d4c38422d8d
Hendi Santika <hendisantika@gmail.com>
2022-03-23 07:19:58 +0700

    #4 - Add Fauna Config

---
+++ src/main/java/com/hendisantika/config/FaunaConfiguration.java
@@ -0,0 +1,34 @@
+package com.hendisantika.config;
+
+import com.faunadb.client.FaunaClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-faunadb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/03/22
+ * Time: 07.19
+ */
+@Configuration
+class FaunaConfiguration {
+    @Value("https://db.${fauna.region}.fauna.com/")
+    private String faunaUrl;
+
+    @Value("${fauna.secret}")
+    private String faunaSecret;
+
+    @Bean
+    FaunaClient getFaunaClient() throws MalformedURLException {
+        return FaunaClient.builder()
+                .withEndpoint(faunaUrl)
+                .withSecret(faunaSecret)
+                .build();
+    }
+}
